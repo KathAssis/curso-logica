4b19c072d496b0a39824aa9d7e712de808acfc5b
KathAssis <kathlynprado44@gmail.com>
2024-08-28 21:03:21 -0300

    Create exec-4.py

---
+++ exec-4.py
@@ -0,0 +1,8 @@
+4. Quadrado - Escrever um programa que mostre a seguinte figura:
+
+print("Escreva um programa que mostre a seguinte figura")
+print("XXXXX")
+print("X   X")
+print("X   X")
+print("X   X")
+print("XXXXX")
