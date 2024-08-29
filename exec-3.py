9aeb0878035f8886fff7422d0fb4f1cb201472df
KathAssis <kathlynprado44@gmail.com>
2024-08-28 21:01:47 -0300

    Create exec-3.py

---
+++ exec-3.py
@@ -0,0 +1,4 @@
+3. Letra de música - Faça um programa que mostre na tela uma letra de música que você gosta (proibido letras do Justin Bieber).
+
+
+print("Letra de uma música que você goste é:This girl is on fire this girl is on fire she's walking on fire this girl is on fire")
